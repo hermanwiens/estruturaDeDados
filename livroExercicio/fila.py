7a9d6df57530513478ea200e3d9cafe53903175e
hermanwiens <136393033+hermanwiens@users.noreply.github.com>
2026-03-18 22:46:44 +0000

    livro

---
+++ livroExercicio/fila.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self._itens = []
+    
+    def is_empty(self):
+        return len(self._itens) == 0
+    
+    def enqueue(self, item):#enqueue append acressenta um item no final da lista
+        self._itens.append(item)
+    
+    def dequeue(self):#dequeue pop tira da fila 
+        if not self.is_empty():
+            return self._itens.pop(0)
+        else:
+            raise IndexError("A fila está vazia.")
+    
+    def size(self):
+        return len(self._itens)
+    
+    def peek(self):
+        if not self.is_empty():
+            return self._itens[0]
+        else:
+            raise IndexError("A fila está vazia.")
+    def __str__(self):
+        return str(self._itens)
