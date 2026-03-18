d6f054b62399962cdaf32a3698bb287e3f6af2de
hermanwiens <136393033+hermanwiens@users.noreply.github.com>
2026-03-18 23:48:55 +0000

    livrocerto

--- livroExercicio/livroFila.py
+++ livroExercicio/livroFila.py
@@ -6,19 +6,20 @@ class livroFila:
         self.fila = Queue()
         self.livros_lidos = []
 
-def adicionar_livro(self, livro):
+    def adicionar_livro(self, livro):
         if not isinstance(livro, Livro):
             raise TypeError("Apenas objetos do tipo Livro podem ser adicionados")
         self.fila.enqueue(livro)
         print("Adicionando ", livro)
  
-def marcarLivroLido(self):
+    def marcarLivroLido(self):
         if self.fila.is_empty():
             print("Não há livros na fila para marcar como lido.")
             return
         livro_lido = self.fila.dequeue()
         print("Livro marcado como lido: ", livro_lido)
-def mostraLivros(self):
+
+    def mostraLivros(self):
         if self.fila.is_empty():
             print("Não há livros na fila.")
             return
